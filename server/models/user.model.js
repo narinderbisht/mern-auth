915ca6222ef8a2a3e1b54d31644d1b882359ea15
narinderbisht <narinderbisht1989@gmail.com>
2023-11-24 20:14:46 +0530

    created user model

---
+++ server/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

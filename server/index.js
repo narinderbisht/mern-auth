4e8c761fdae3813be6fb0176ee9339b876ca9791
narinderbisht <narinderbisht1989@gmail.com>
2023-11-24 19:58:39 +0530

    mongodb connection updated

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+
+const db = mongoose.connect(process.env.MONGODB).then( () => {
+    console.log("Database connection is working");
+}).catch(( err ) => {
+    console.log("Database is not working. Error: " + err);
+})
 
 app.listen(3000, () => {
     console.log("server is running on port 3000.");

5804ba27bd62333e59976516b1104f542fa9251b
narinderbisht <narinderbisht1989@gmail.com>
2023-11-25 16:52:00 +0530

    signin API route created

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async(req, res, next) => {
     //console.log(req.body);
@@ -22,4 +23,29 @@ export const signup = async(req, res, next) => {
         // next(err); // can use as middelware
         //res.status(500).json(err.message);
     }
+}
+
+export const signin = async (req, res, next) => {
+    console.log(req.body);
+    try {
+        const validUser = await User.findOne({ email: req.body.email });
+        if (!validUser) {
+            return next(errorHandler(404, 'User is not found'));
+        }
+        const validPassword = bcryptjs.compareSync(req.body.password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, 'Invalid credentials'));
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hasedPassword, ...rest } = validUser._doc;
+
+        res.cookie('access_token', token, { httpOnly: true, expire: new Date(Date.now()) + 3600000 }).status(201).json( rest );
+
+    } catch (error) {
+        //next(errorHandler(500, error.message));
+        next(error);
+    }
+        
+    
 }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const authRoutes = express.Router();
 
 authRoutes.post('/signup', signup);
+authRoutes.post('/signin', signin);
 
 export default authRoutes;

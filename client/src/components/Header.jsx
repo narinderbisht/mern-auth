cee3337b84e308ea34a8007dd18d54133398a1c2
narinderbisht <narinderbisht1989@gmail.com>
2023-11-26 17:02:39 +0530

    Profile page template ready

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link, Route } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  //console.log(currentUser);
   return (
       <div className='bg-slate-200'>
           <div className='flex justify-between max-w-6xl mx-auto items-center p-2'>
@@ -10,7 +13,11 @@ export default function Header() {
                 <ul className='flex gap-4'>
                     <li><Link to={'/'}>Home</Link></li>
                     <li><Link to={'/about'}>About</Link></li>
-                    <li><Link to={'/sign-in'}>Sign IN</Link></li>
+                    { currentUser ? 
+                      <li><Link to={'/profile'}><img src={currentUser.profilePicture} className="rounded-full h-7 w-7 outline"/></Link></li>
+                    : <li><Link to={'/sign-in'}>Sign IN</Link></li>
+                    }
+                    
                 </ul>
           </div>
     </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [ loading, setLoading ] = useState('');
+  const handleSubmit = () => {
+
+  }
   return (
-    <div>Profile</div>
+    <div className='max-w-lg mx-auto'>
+      <h1 className='font-semibold text-center text-xl mt-4'>Profile</h1>
+      
+      <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+      <img src={currentUser.profilePicture} className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2'/>
+        <input type="text" placeholder="Username" id="username" className='p-3 rounded-lg bg-slate-100' defaultValue={currentUser.username} />
+        <input type="email" placeholder="Email" id="email" className='p-3 rounded-lg bg-slate-100' defaultValue={currentUser.email} />
+        <input type="password" placeholder="Password" id="password" className='p-3 rounded-lg bg-slate-100' />
+        <button className='uppercase rounded-lg bg-slate-500 p-3 text-center text-white hover:opacity-90 disabled:opacity-50'>{ loading ? 'Loading...': 'Update Profile'}</button>
+      </form>
+      <div className='flex justify-between mt-4'>
+        <span className='text-red-500 cursor-pointer'>Delete Account</span>
+        <span className='text-red-500 cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
   )
 }
